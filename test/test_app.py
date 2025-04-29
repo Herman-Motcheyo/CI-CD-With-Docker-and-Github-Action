34f6c019a91f17ab336b693751f142b88b84314e
Herman-Motcheyo <hermanmotcheyo@gmail.com>
2025-04-29 05:14:25 +0100

    bug fix into test file

--- test/test_app.py
+++ test/test_app.py
@@ -8,6 +8,10 @@ from app import app
 
 def test_home():
     response = app.test_client().get("/")
-
-    assert response.status_code==200
-    assert response.data==b"Hello World!"
+    expected_html = b"""
+    <h1>Welcome to My First CI/CD Project! </h1>
+    <p>This application demonstrates a simple deployment using Docker and GitHub Actions.</p>
+    <p>Hello, World! </p>
+    """
+    assert response.status_code == 200
+    assert response.data == expected_html
