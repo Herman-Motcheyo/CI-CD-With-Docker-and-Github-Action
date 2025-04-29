421f8073d8f9ba0cc4e50dd42740b6cbce14a0bb
Herman-Motcheyo <hermanmotcheyo@gmail.com>
2025-04-29 04:46:37 +0100

    modify app with new message

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask
 
-app=Flask(__name__)
+app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello World!"
+    return """
+    <h1>Welcome to My First CI/CD Project! </h1>
+    <p>This application demonstrates a simple deployment using Docker and GitHub Actions.</p>
+    <p>Hello, World! </p>
+    """
 
-if __name__=="__main__":
-    app.run(host="0.0.0.0",port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
